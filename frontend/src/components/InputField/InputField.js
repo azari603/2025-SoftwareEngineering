2e00fb89b1e051422e9ab8fe2e5e43f26ae65cf5
sujin <ssjjlee22@gmail.com>
2025-09-24 16:05:05 +0900

    feat: Input 박스 컴포넌트 추가

---
+++ frontend/src/components/InputField/InputField.js
@@ -0,0 +1,29 @@
+// InputField.jsx
+import React from "react";
+import "./InputField.css";
+
+const InputField = ({ 
+  label, 
+  type = "text", 
+  placeholder, 
+  required = false, 
+  value, 
+  onChange 
+}) => {
+  return (
+    <div className="input-field">
+      <label>
+        {label} {required && <span className="required">*</span>}
+      </label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        required={required}
+      />
+    </div>
+  );
+};
+
+export default InputField;
