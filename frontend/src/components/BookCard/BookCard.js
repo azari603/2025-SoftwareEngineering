184b2725a4707687d55a1e96e739caf2ad69c599
sujin <ssjjlee22@gmail.com>
2025-09-27 22:32:44 +0900

    feat: BookCard 사이즈 조절 추가

--- frontend/src/components/BookCard/BookCard.js
+++ frontend/src/components/BookCard/BookCard.js
@@ -2,15 +2,30 @@ import { forwardRef } from "react";
 import { Link } from "react-router-dom";
 import "./BookCard.css";
 
-const BookCard = forwardRef(({ book }, ref) => {
+const sizeToVars = {
+  sm: { cardW: 120, imgH: 160 },
+  md: { cardW: 150, imgH: 200 }, // 기본값
+  lg: { cardW: 180, imgH: 260 },
+};
+
+const BookCard = forwardRef(({ book, size = "lg", cardWidth, imageHeight }, ref) => {
+  const preset = sizeToVars[size] || sizeToVars.md;
+
+  const styleVars = {
+      // 프리셋을 기본으로, 개별 값을 넘기면 그걸 우선
+      "--card-w": (cardWidth ?? preset.cardW) + "px",
+      "--img-h": (imageHeight ?? preset.imgH) + "px",
+    };
+
+
   return (
-    <Link to={`/book/${book.isbn}`} className="book-card" ref={ref}>
+    <Link to={`/book/${book.isbn}`} className={`book-card ${size}`} ref={ref} style={styleVars}>
       <div className="book-image">
         <img src={book.image} alt={book.title} />
       </div>
       <div className="book-info">
-        <h3>{book.title || "책제목"}</h3>
-        <p>{book.author || "저자이름"}</p>
+        <h3 className="book-title">{book.title || "책제목"}</h3>
+        <p className="book-author">{book.author || "저자이름"}</p>
       </div>
     </Link>
   );
