739a8b605d00c138da319258455ea760d121069c
sujin <ssjjlee22@gmail.com>
2025-09-28 02:58:01 +0900

    feat: home 라우터 연결 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,49 @@
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header/Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+import SignupLayout from "./pages/SignupPage/SignupLayout";
+import SignupSuccess from "./pages/SignupPage/SignupSuccess/SignupSuccess";
+import SignupEmail from "./pages/SignupPage/SignupEmail/SignupEmail";
+import Home from "./pages/Home/Home";
+import { dummyBooks } from "./mocks/dummyBooks";
+import { dummyReviews } from "./mocks/dummyReviews";
 
-function App() {
+export default function App() {
+  const isLoggedIn = false;
+  const nickname = "빙봉";
+
+  const todaysBooks = dummyBooks;
+  const todaysReviews = dummyReviews;
+  const recommendedBooks = dummyBooks;
+  const followingReviews = dummyReviews;
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header/>
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              isLoggedIn={isLoggedIn}
+              nickname={nickname}
+              todayBooks={todaysBooks}
+              todayReviews={todaysReviews}
+              recommendedBooks={recommendedBooks}
+              followingReviews={followingReviews}
+            />
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupLayout />}>
+        <Route index element={<SignupPage />} />
+        <Route path="success" element={<SignupSuccess />} />
+        <Route path="email" element={<SignupEmail />} />
+        </Route>
+      </Routes>
+    </Router>
+    
   );
 }
 
-export default App;
+
+
