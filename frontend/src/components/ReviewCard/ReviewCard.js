c8c9be35ae145ba63b9b67fad6993ab226937079
sujin <ssjjlee22@gmail.com>
2025-09-27 22:39:43 +0900

    feat: ReviewList 추가 및 Carousel 레이아웃 수정

--- frontend/src/components/ReviewCard/ReviewCard.js
+++ frontend/src/components/ReviewCard/ReviewCard.js
@@ -1,39 +1,39 @@
+import { forwardRef } from "react";
 import { Link } from "react-router-dom";
-import "./ReviewCard.css";
 import starIcon from "../../assets/star.png"
+import "./ReviewCard.css"
 
-const ReviewCard = ({ review, variant = "basic" }) => {
+const ReviewCard = forwardRef(({ review, variant = "basic" }, ref) => {
   return (
-    <Link to={`/review/${review.id}`} className={`review-card ${variant}`}>
-        {/*책 정보 포함*/}
+    <Link to={`/review/${review.id}`} className={`review-card ${variant}`} ref={ref}>
       {variant === "withBook" && review.book && (
         <div className="review-book-section">
-        <img
-          src={review.book.image}
-          alt={review.book.title}
-          className="review-book-image"
-        />
-        <h4 className="review-book-title">{review.book.title}</h4>
-        <p className="review-book-author">{review.book.author}</p>
-      </div>
+          <img
+            src={review.book.image}
+            alt={review.book.title}
+            className="review-book-image"
+          />
+          <h4 className="review-book-title">{review.book.title}</h4>
+          <p className="review-book-author">{review.book.author}</p>
+        </div>
       )}
 
       <div className="review-body">
         <div className="review-header">
           <h3 className="review-title">{review.title} {review.subtitle}</h3>
-          <span className="review-rating"> 
-            <img src={starIcon} alt="별점" className="review-star"/>
-            {review.rating}/5</span>
+          <span className="review-rating">
+            <img src={starIcon} alt="별점" className="review-star" />
+            {review.rating}/5
+          </span>
         </div>
         <p className="review-user">{review.user}</p>
         <p className="review-preview">
-          {review.preview.length > 80
-            ? review.preview.slice(0, 80) + "... 더 보기"
-            : review.preview}
+          {review.preview.length > 80 ? review.preview.slice(0, 80) + "... 더 보기" : review.preview}
         </p>
       </div>
     </Link>
   );
-};
+});
 
 export default ReviewCard;
+
